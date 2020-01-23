b63f58c32ec66a779db51c3a36e8c693e210cda1
Varun Aggarwal <salazar.prime007@gmail.com>
2020-01-23 14:52:03 -0500

    done lab2

--- program_4.2.py
+++ program_4.2.py
@@ -1,9 +1,16 @@
+""" Program to draw flowers
+
+Implementation of Exercise 4.2
+Book: Think Python 2nd Edition by Allen B. Downey
+Edition: 2e
+Link: https://greenteapress.com/wp/think-python-2e/
+"""
+
 import turtle
 import math
 
 def polyline(t, n, length, angle):
     """Draws n line segments.
-
     t: Turtle object
     n: number of line segments
     length: length of each segment
@@ -15,7 +22,6 @@ def polyline(t, n, length, angle):
 
 def arc(t, r, angle):
     """Draws an arc with the given radius and angle.
-
     t: Turtle
     r: radius
     angle: angle subtended by the arc, in degrees
@@ -33,7 +39,6 @@ def arc(t, r, angle):
 
 def flower(n, r, a, t):
 	""" Draw flower with n petals 
-
 	n: number of petals
 	a: angle turtle moves before turning 
 	r: radius of petal
@@ -53,7 +58,43 @@ def flower(n, r, a, t):
 		i += 1 # outer loop counter
 
 whatsupdoc = turtle.Turtle()
-flower(7, 50, 60, whatsupdoc)
+whatsupdoc.speed('fastest')
+
+
+""" flower 1 """
+
+# move to new spot
+whatsupdoc.pu()
+whatsupdoc.fd(-200)
+whatsupdoc.pd()
+#start drawing
+flower(7, 100, 60, whatsupdoc)
+
+
+
+""" flower 2 """
+
+# move to new spot
+whatsupdoc.pu()
+whatsupdoc.fd(200)
+whatsupdoc.pd()
+#start drawing
+flower(10, 80, 80, whatsupdoc)
+
+
+""" flower 3 """
+
+# move to new spot
+whatsupdoc.pu()
+whatsupdoc.fd(200)
+whatsupdoc.pd()
+#start drawing
+flower(20, 260, 20, whatsupdoc)
 turtle.mainloop()
 
 
+"""
+Author: Varun Aggarwal
+Username: aggarw82
+Github: https://github.com/Environmental-Informatics/building-more-complex-programs-with-python-aggarw82
+"""
