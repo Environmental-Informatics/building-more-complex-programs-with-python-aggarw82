43011ca97dddc1f2921a4127167dbcc8ee8f26cc
Varun Aggarwal <salazar.prime007@gmail.com>
2020-01-23 14:11:04 -0500

    in progres turtle

--- program_4.2.py
+++ program_4.2.py
@@ -1,1 +1,59 @@
-import 
+import turtle
+import math
+
+def polyline(t, n, length, angle):
+    """Draws n line segments.
+
+    t: Turtle object
+    n: number of line segments
+    length: length of each segment
+    angle: degrees between segments
+    """
+    for i in range(n):
+        t.fd(length)
+        t.lt(angle)
+
+def arc(t, r, angle):
+    """Draws an arc with the given radius and angle.
+
+    t: Turtle
+    r: radius
+    angle: angle subtended by the arc, in degrees
+    """
+    arc_length = 2 * math.pi * r * abs(angle) / 360
+    n = int(arc_length / 4) + 3
+    step_length = arc_length / n
+    step_angle = float(angle) / n
+
+    # making a slight left turn before starting reduces
+    # the error caused by the linear approximation of the arc
+    t.lt(step_angle/2)
+    polyline(t, n, step_length, step_angle)
+    t.rt(step_angle/2)
+
+def flower(n, r, a, t):
+	""" Draw flower with n petals 
+
+	n: number of petals
+	a: angle turtle moves before turning 
+	r: radius of petal
+	"""
+	i = 0
+	while i != n:
+
+		# loop to draw petal
+		j = 1
+		while j != 3:
+			arc(t, r, a)
+			t.lt(180 - a) # turn turtle for second half of petal
+			j += 1
+
+		# turn tutle for next petal
+		t.lt(360.0/n)
+		i += 1 # outer loop counter
+
+whatsupdoc = turtle.Turtle()
+flower(7, 50, 60, whatsupdoc)
+turtle.mainloop()
+
+
